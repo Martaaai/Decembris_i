b0be3ee86b50cca0cf48dd33cfd1467b906c8810
Martaaai <marta.indriksone@grobinasskola.lv>
2023-11-23 12:36:36 +0200

    message

--- 1.uzdevums.py
+++ 1.uzdevums.py
@@ -1,3 +1,4 @@
+#Izveidojiet Python programmu, kas saskaita no 1 līdz lietotāja ievadītam skaitlim, izmantojot for ciklu.
 sk1=int(input("1"))
 sk1=0
 while sk1 <=100:
---
+++ 2.uzdevums.py
@@ -0,0 +1,8 @@
+#Izveidojiet Python programmu, kas izmanto while ciklu, lai atrastu pirmo skaitli, kura kvadrāts ir lielāks par 1000.
+skait=0
+while skait>=1000:
+        print(skait)
+    
+
+
+
